b94d58d557d07e377b262ea6f016e01d13ed313e
Kristóf Poduszló <kripod@protonmail.com>
2017-01-12 13:21:11 +0100

    feat(demo): Add FAB component

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,11 +2,13 @@ import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
 import ButtonDemo from './ButtonDemo';
+import FabDemo from './FabDemo';
 import './index.css';
 
 render(
   <main>
     <ButtonDemo />
+    <FabDemo />
   </main>,
   document.getElementById('root')
 );
