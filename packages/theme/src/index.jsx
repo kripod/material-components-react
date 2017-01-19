a651b7d07308f8f395daffb4cd7b0093c323e3c8
Kristóf Poduszló <kripod@protonmail.com>
2017-01-19 13:59:09 +0100

    feat(theme): Add theme component

---
+++ packages/theme/src/index.jsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import React, { PropTypes } from 'react';
+
+const Theme = (props) => {
+  const { className, color, backgroundColor, dark, ...restProps } = props;
+
+  const colorClassNames = color.split(' ');
+  const backgroundColorClassNames = backgroundColor.split(' ');
+
+  return (
+    <div
+      {...restProps}
+      className={classNames(
+        colorClassNames.map(cn => `mdc-theme--${cn}`),
+        backgroundColorClassNames.map(cn => `mdc-theme--${cn}-bg`),
+        { 'mdc-theme--dark': dark },
+        className,
+      )}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+Theme.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  dark: PropTypes.bool,
+};
+
+Theme.defaultProps = {
+  className: '',
+  color: '',
+  backgroundColor: '',
+  dark: false,
+};
+
+export default Theme;
